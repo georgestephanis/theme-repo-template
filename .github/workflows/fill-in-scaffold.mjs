1028eb0a6fca74bb192e688884ccc12b033e971a
George Stephanis <daljo628@gmail.com>
2025-08-11 18:50:14 -0400

    Populate fill in scaffold stuff.

---
+++ .github/workflows/fill-in-scaffold.mjs
@@ -0,0 +1,69 @@
+import { statSync } from 'fs';
+import { readdir, readFile } from 'fs/promises';
+import { writeFile } from 'fs/promises';
+import { join as joinPath } from 'path';
+import process from 'process';
+
+const repository = JSON.parse( process.argv[2] );
+const skip_dirs = [ '.github', '.git' ];
+
+/**
+ * @param {string} dirPath
+ * @param {(filePath: string) => Promise<void>} callback
+ */
+const traverseDirectory = async ( dirPath, callback ) => {
+	if ( skip_dirs.includes( dirPath ) ) {
+		console.log( 'Skipping %s', dirPath );
+		return;
+	}
+	console.log( 'Traversing %s', dirPath );
+
+	const files = await readdir( dirPath ); // Read the contents of the directory
+	for ( const file of files ) {
+		const filePath = joinPath( dirPath, file );
+
+		if ( statSync( filePath ).isFile() ) {
+			await callback( filePath );
+		} else { // Recursively traverse directories
+			await traverseDirectory( filePath, callback );
+		}
+	}
+};
+
+/**
+ * Build a template using envs
+ * @param {string} filePath
+ */
+const buildTemplate = async ( filePath ) => {
+	console.log( 'Building %s', filePath );
+
+	const templateFile   = await readFile( filePath, 'utf-8' );
+	let renderedTemplate = templateFile, replacements;
+
+	if ( 'README.md' === filePath ) {
+		replacements = {
+			'EXAMPLE_REPO_NAME': repository.name,
+			'EXAMPLE_REPO_DESCRIPTION': repository.description ?? '',
+			'EXAMPLE_REPO_URL': repository.homepage ?? 'https://wordpress.org',
+		};
+	} else {
+		replacements = {
+			'Theme Repo Template': ( String(repository.name).replace( '-', ' ' ).toLocaleUpperCase() ),
+            'ThemeRepoTemplate': ( String(repository.name).replace( '-', ' ' ).toLocaleUpperCase().replace( ' ', '' ) ),
+			'theme-repo-template': repository.name.toLowerCase(),
+			'A starter theme for FSE.  Will generally be overwritten.': repository.description ?? 'A spiffy new theme.',
+			'https://github.com/georgestephanis/theme-repo-template': repository.homepage ?? 'https://wordpress.org',
+		};
+	}
+
+	for ( const [ key, value ] of Object.entries( replacements ) ) {
+		renderedTemplate = renderedTemplate.replaceAll( key, value );
+	}
+
+	if ( renderedTemplate !== templateFile ) {
+		console.log( 'Changes were made. Overwriting file.' );
+		await writeFile( filePath, renderedTemplate );
+	}
+};
+
+await traverseDirectory( '.', buildTemplate );
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# theme-repo-template
-
+# Theme Repo Template
 A repository to create theme repositories with built tools pre-configured.
+
+Use this repository as a template, and then populate a couple arguments and run the Repository Workflow to fill in the scaffold (in the generated repository, not this one!)

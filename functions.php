41f16ce2d5782dd2a0c79efc0498b5cd960f06e2
George Stephanis <daljo628@gmail.com>
2025-08-11 17:28:19 -0400

    First basic hot take.

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Theme Editor Template theme functions and definitions.
+ *
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package ThemeEditorTemplate_Theme
+ */
+
+namespace ThemeEditorTemplate\Theme;
+
+/**
+ * Handle addition of any enqueues for the front-end.
+ *
+ * @return void
+ */
+function enqueue_block_assets() {
+	// Handle adding the theme's style.css for generic non-block-specific styles.
+	\wp_enqueue_style(
+		'theme-repo-template',
+		\get_stylesheet_uri(),
+		array(),
+		(string) filemtime( __DIR__ . '/style.css' )
+	);
+}
+\add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_block_assets' );
+
+/**
+ * Handle addition of any enqueues for the block editor only.
+ *
+ * @return void
+ */
+function enqueue_block_editor_assets() {
+	\wp_enqueue_script(
+		'theme-repo-template',
+		\get_theme_file_uri( 'js/block-editor.js' ),
+		array(),
+		(string) filemtime( __DIR__ . '/js/block-editor.js' ),
+		true
+	);
+}
+\add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
